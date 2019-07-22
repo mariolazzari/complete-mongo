574d5c4175d353b4cc30291570165e8f2b538775
Mario Lazzari <mario.lazzari@gmail.com>
2019-07-22 19:38:40 +0200

    Done lesson 15

--- users/test/test_he,per.js
+++ users/test/test_he,per.js
@@ -1,3 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhist/users_test");
+mongoose.connection
+  .once("open", () => console.log("Good to go."))
+  .on("error", error => console.warn("Warning", error));
